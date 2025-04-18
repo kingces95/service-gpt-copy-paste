33fc0a30ce0d3842745f223df5530af386bb5c4b
Chris King <kingces95@gmail.com>
2025-04-18 09:34:50 -1000

    checkpoint

--- src/clippy/main.js
+++ src/clippy/main.js
@@ -1,11 +1,9 @@
-import { CliCommand } from '@kingjs/cli-command'
+import { CliTerminal } from '@kingjs/cli-terminal'
 import { CliRxPollerState } from '@kingjs/cli-rx-poller'
 import { CliRuntimeState } from '@kingjs/cli-runtime'
 import { CliDaemon, CliDaemonState } from '@kingjs/cli-daemon'
-import { CliTerminal } from '@kingjs/cli-terminal'
 import { CliPulse } from '@kingjs/cli-pulse'
 import { CliConsole, CliConsoleMon } from '@kingjs/cli-console'
-import { pipeline } from 'stream/promises'
 
 // e.g., !#/clipboard/shell/bash echo hello world
 // e.g., !#/clipboard/http/get https://example.com
@@ -40,16 +38,20 @@ export default class Main extends CliTerminal {
     //   stdout: shell.$`this dispatch`,
     // })()
 
+    // const result = await shell.$`this dispatch`({
+    //   stdin: shell.$`this poll --stdmon /dev/null`,
+    // })()
+
     const result = await shell.pipeline(
       shell.$`this poll --stdmon /dev/null`,
       shell.$`this dispatch`,
     )
 
-    // const result = await shell.subshell(shell => {
-    //   shell.$`this dispatch`()
+    // const result = await shell.subshell(async shell => {
+    //   return await shell.$`this dispatch`()
     // })({ 
     //   stdin: shell.$`this poll --stdmon /dev/null`, 
-    // })
+    // })()
 
     //while (signal.aborted == false) {
       // shell.pipeline(
